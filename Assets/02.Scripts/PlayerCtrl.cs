49db97fe569cd3e8b47cc608eae1e57268c144ef
SJ9211 <jsj921127@naver.com>
2025-01-17 11:12:46 +0900

    Player

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class PlayerCtrl : MonoBehaviour
 {
+    int gv = 0;
+
+    void Awake()
+    {
+        gv =1;
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!!!");
+        gv = 100;
     }
 
     // Update is called once per frame
