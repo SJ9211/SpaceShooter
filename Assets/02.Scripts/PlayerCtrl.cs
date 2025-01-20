a1a79434d924fd4ea8896563d3778d76f44657d5
SJ9211 <jsj921127@naver.com>
2025-01-20 15:48:43 +0900

    PlayerCtrl

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    int gv = 0;
-
-    void Awake()
-    {
-        gv =1;
-    }
-    
-    
-    // Start is called before the first frame update
+    // 컴포넌트를 캐시 처리할 변수
+    private Transform tr;
     void Start()
     {
-        gv = 100;
+        // Transform 컴포넌트를 추출해 변수에 대입
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
+        float v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ + 1.0f
+
+        Debug.Log("h=" + h);
+        Debug.Log("v=" + v);
+
+        //  Transform 컴포넌트의 position 속성값을 변경
+        // transform.position += new Vector3(0, 0, 1);
+
+        // 정규화 백터를 사용한 코드
+        transform.position += Vector3.forward * 1;
     }
 }
