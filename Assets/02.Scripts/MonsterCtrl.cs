b4080bed961aff070e63781c48f849fb03d74e45
SJ9211 <jsj921127@naver.com>
2025-02-24 15:50:54 +0900

    Monster

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -10,6 +10,8 @@ public class MonsterCtrl : MonoBehaviour
     private readonly int hashTrace = Animator.StringToHash("IsTrace");
     private readonly int hashAttack = Animator.StringToHash("IsAttack");
     private readonly int hashHit = Animator.StringToHash("Hit"); 
+    private readonly int hashPlayerDie = Animator.StringToHash("PlayerDie");
+    private readonly int hashSpped = Animator.StringToHash("Spped");
     public const float TIMER_CHECK = 0.3f;
     public enum State
     {
@@ -129,5 +131,20 @@ public class MonsterCtrl : MonoBehaviour
         // 혈흔 효과 생성
         GameObject blood = Instantiate<GameObject>(bloodEffect, pos, rot, monsterTr);
         Destroy(blood, 1.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+
+    void OnPlayerDie()
+    {
+        // 몬스터의 상태를 체크하는 코루틴 함수를 모두정지
+        StopAllCoroutines();
+        // 추적을 정지하고 애니메이션을 수행
+        agent.isStopped = true;
+        anim.SetFloat(hashSpped, Random.Range(0.8f, 1.2f));
+        anim.SetTrigger(hashPlayerDie);
     }
 }
